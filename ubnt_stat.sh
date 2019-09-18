8836f46f5d9fb6330072232bbd2172eea8bdd881
DL 7 ATA <dl7ata@users.noreply.github.com>
2019-09-18 16:22:30 +0200

    Add files via upload

---
+++ ubnt_stat.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ubnt_stat.sh
+# Auslesen der Feldstärke einer verbundenen Ubiquity Station
+# Vorher muss auf der GEgenstation der SSH-Schlüssel abgelegt werden (ssh-copy)
+# by DL 7 ATA
+#
+# V 1 16.07.2019
+# Abruf M2
+ohne_verbindung='\[ \]'
+SUCHE='\"rssi\": '
+datei="/tmp/ubnt.stat"
+
+while true; do
+
+ssh -x ubnt@192.168.10.52 "cat /var/tmp/stats/wstalist" > $datei
+rx=$(cat $datei)
+
+if [ "$rx" == "$ohne_verbindung" ]; then
+   echo "Kein Verbindung ...."
+   echo $rx
+else
+   signal=$(cat $datei | grep "\"rssi")
+   signal1=$(echo $signal | grep $SUCHE | cut -d"," -f1 | cut -d":" -f2)
+   signal2=$(echo $signal | grep $SUCHE | cut -d"," -f2 | cut -d":" -f2)
+   echo -e "$(date +%H:%M:%S) RX:${signal1}dB TX:${signal2}dB"
+
+fi
+sleep 3
+done
+
+rm $datei
+
+exit 0
