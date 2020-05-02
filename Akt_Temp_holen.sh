7944637db143bc2a32888fb6374155b8dcb1ab35
FKo <dl7ata@freenet.de>
2020-05-02 22:34:01 +0200

    Update

--- Akt_Temp_holen.sh
+++ Akt_Temp_holen.sh
@@ -5,11 +5,11 @@
 # Script zur Erzeugung einer aktuellen Temperaturmeldung, wird per cronjob regelmäßig aufgerufen und stellt 
 # u.a. für /usr/share/svxlink/events.d/local/Logic.tcl Temperatur als Textvariable zur Verfügung
 #
-# 30.8.2018
-# letzte Überarbeitung und Anpassung log-Ausgabe und mehr
+# 10.04.2019	Einfügen Timeout-Parameter bei curl-Aufruf
 #
-# 18-12-2016
-# v1.4  8.1.2018
+# 30.8.2018	letzte Überarbeitung und Anpassung log-Ausgabe und mehr
+#
+# 18-12-2016 	v1.4  8.1.2018
 # Ausgabe des Wertes für APRS-Statistik: "APRS.TempLuft"
 #
 # v1.3 vom 4.10.2017
@@ -31,21 +31,19 @@ svx_text="set"
 #------------------extrahieren des Textes-----------------------------------------------------------------------
 
 # und prüfen zunächst, ob die Seite erreichbar ist ...
-Status_code=$(curl --output /dev/null --silent --head --write-out '%{http_code}\n' "$server")
-echo "Statuscode für $server: $Status_code"
+Status_code=$(curl --max-time 5,0 --output /dev/null --silent --head --write-out '%{http_code}\n' "$server")
+echo -e " $(date)\nStatuscode für $server: $Status_code"
 
  # wenn ja = 200
  if [ $Status_code = "200" ]; then
 	echo "Hole WX Daten von $server für $LK"
-	# Die Datei $PFAD/temp.EDDT liegt jetzt als HTML lokal vor.
-	curl -s  --max-time 10 "$server$serv_str" > $PFAD/temp.$LK
+	cont=$(curl -s  --max-time 10 "$server$serv_str")
 	# Extrahieren aus der HTML-Seite mit sed:
-	msg=$(cat $PFAD/temp.$LK |  grep '<temp_c>' | sed 's/<temp_c>/temp /g' | sed 's/<\/temp_c>//g' | sed 's/^[ \t]*//' | cut -d. -f1 )
+	msg=$(echo -n "$cont" |  grep '<temp_c>' | sed 's/<temp_c>/temp /g' | sed 's/<\/temp_c>//g' | sed 's/^[ \t]*//' | cut -d. -f1 )
 
 	if [ -n "$msg" ]; then
 		echo "$svx_text $msg" > $PFAD/svx_akt_temp
 		echo "$msg" | cut -c 6- > $PFAD/APRS.TempLuft
-		rm $PFAD/temp.$LK
 	else
 		# Alternative Quelle
 		echo "APRS holen weil GOV nicht erreichbar."
